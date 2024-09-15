cce5c7593ac32f254f6e5dd83f3e565b210e590c
JiliJiang <jiangjili@gmail.com>
2024-09-15 14:18:24 -0700

    update

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
         Program myTest = new Program();
-        int[] result = myTest.twoNumberSum( new int[]{3, 5, -4, 8, 11, 1, -1, 6}, 10);
+        int[] result = myTest.twoNumberSum( new int[]{3, 5, -4, 8, 11, 1, -1, 6}, 2);
+        int[] resultNew = myTest.twoNumberSumNew( new int[]{3, 5, -4, 8, 11, 1, -1, 6}, 2);
+        int[] resultList = myTest.twoNumberSumList( new int[]{3, 5, -4, 8, 11, 1, -1, 6}, 2);
+
         String resultString = Arrays.toString(result);
-        System.out.println(resultString);
+        String resultStringNew = Arrays.toString(resultNew);
+        String resultStringList = Arrays.toString(resultList);
+        System.out.println(resultString + ' ' + resultStringNew +" "+ resultStringList);
     }
 }
--- src/Program.java
+++ src/Program.java
@@ -1,7 +1,9 @@
 import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Program {
-    public  int[] twoNumberSum(int[] array, int targetSum){
+    public int[] twoNumberSum(int[] array, int targetSum){
         int[] result = new int[2];
         for (int i=0; i< array.length; i++) {
             int remainder = targetSum - array[i];
@@ -15,7 +17,41 @@ public class Program {
         return result;
     }
 
-    public int getIndex(int[] array, int num){
+    public int[] twoNumberSumNew (int[] array, int targetSum){
+        int[] result = {-1, -1};
+        for (int num: array) {
+            int remainder = targetSum - num;
+            int indexR = getIndex(array, remainder);
+            int indexNum = getIndex(array, num);
+            if(indexR == -1 || indexR == indexNum){
+                continue;
+            }else{
+                result[0] = num;
+                result[1] = remainder;
+            }
+        }
+        return result;
+    }
+
+    public int[] twoNumberSumList (int[] array, int targetSum){
+        int[] result ={-1, -1};
+        List<Integer> arrayList = this.getArrayList(array);
+        arrayList.forEach(num -> {
+            int remainder = targetSum - num;
+            int indexR = arrayList.indexOf(remainder);
+            int indexN = arrayList.indexOf(num);
+            if(indexR !=-1 && indexR != indexN){
+
+                result[0] = num;
+                result[1] = remainder;
+            }
+        }
+        );
+        return result;
+    }
+
+
+    private int getIndex(int[] array, int num){
         for( int i=0; i<array.length; i++){
             if (array[i] == num){
                 return i;
@@ -23,4 +59,14 @@ public class Program {
         }
         return -1;
     }
+
+    private List<Integer> getArrayList(int[] array){
+        List<java.lang.Integer> arrayList = new ArrayList<>();
+        for (int num : array) {
+            arrayList.add(num);  // Auto-boxes int to Integer
+        }
+        return arrayList;
+    }
+
+
 }
